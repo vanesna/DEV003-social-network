a70c412e7f115ab0e5f764d7bf38e7187c1fbaba
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-01-30 11:47:28 -0500

    se agrego etiqueta form, se agrego password.type y autocomplete en la seccion de login y se enruto el boton register

--- src/components/register.js
+++ src/components/register.js
@@ -1,30 +1,33 @@
-// export const Register = (onNavigate) => {
-//     const main = document.createElement('main');
-//     const imageLogo = document.createElement('img');
-//     imageLogo.src = 'imagenes/logo.png';
-//     const div = document.createElement('div');
+//import { onNavigate } from './main.js';
+
+
+export const Register = (onNavigate) => {
+    const main = document.createElement('main');
+    const imageLogo = document.createElement('img');
+    imageLogo.src = 'imagenes/logo.png';
+    const div = document.createElement('div');
     
-//     const name = document.createElement('input');
-//     name.placeholder = 'Nombre de usuario';
-//     console.log(name, 'nombre');
-//     const email = document.createElement('input');
-//     email.placeholder = 'Correo electrónico';
-//     email.type = 'email';
+    const name = document.createElement('input');
+    name.placeholder = 'Nombre de usuario';
+    console.log(name, 'nombre');
+    const email = document.createElement('input');
+    email.placeholder = 'Correo electrónico';
+    email.type = 'email';
     
-//     const password = document.createElement('input');
-//     password.placeholder = 'Contraseña';
-//     const password2 = document.createElement('input');
-//     password2.placeholder = 'Confirma contraseña';
-//     password2.placeholder = 'Confirma contraseña';
+    const password = document.createElement('input');
+    password.placeholder = 'Contraseña';
+    const password2 = document.createElement('input');
+    password2.placeholder = 'Confirma contraseña';
+    password2.placeholder = 'Confirma contraseña';
     
   
-//     imageLogo.setAttribute('class', 'imagen');
-//     div.setAttribute('class', 'textobienvenida');
-//     textIntro.setAttribute('class', 'intro');
+    imageLogo.setAttribute('class', 'imagen');
+    div.setAttribute('class', 'textobienvenida');
+    textIntro.setAttribute('class', 'intro');
   
-//     div.append(name, email, password, password2, textIntro);
-//     main.append(imageLogo, div);
+    div.append(name, email, password, password2, textIntro);
+    main.append(imageLogo, div);
   
-//     return main;
-//   };
+    return main;
+  };
   
