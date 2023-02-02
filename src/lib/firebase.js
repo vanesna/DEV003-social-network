fb886154c342c6982267523f764d7ca04ebbb45d
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-02-02 09:46:33 -0500

    se agrego evento al boton de registro para que no se recargue la pagina y se cambio la configuracion del firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,24 +3,24 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 // Enlazamos visual con firebase
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC_d9mt29SbKwKTzxAqIJlGr7q-H3i7PiA',
-  authDomain: 'likeplants-c4327.firebaseapp.com',
-  projectId: 'likeplants-c4327',
-  storageBucket: 'likeplants-c4327.appspot.com',
-  messagingSenderId: '456213824098',
-  appId: '1:456213824098:web:fe3fb0aab88bf19d69073b',
-  measurementId: 'G-49HPL6KVN9',
+  apiKey: 'AIzaSyAXa8j4Ybc510kv68WeHddFaKFWOXkqhsM',
+  authDomain: 'likeplants-bc662.firebaseapp.com',
+  projectId: 'likeplants-bc662',
+  storageBucket: 'likeplants-bc662.appspot.com',
+  messagingSenderId: '1094663123741',
+  appId: '1:1094663123741:web:439a81d9b80773221527f4',
+  measurementId: 'G-LX9KPQYCP0',
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
 // Crear nueva cuenta pasando la dirección de correo electrónico y la contraseña del nuevo usuario
-const auth = getAuth(app);
+const auth = getAuth(app); // constante para poder autenticar usuarios
 export const register = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
 
