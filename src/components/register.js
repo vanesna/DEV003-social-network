71d8e0f776f73f595f70af7f2cffb25d07a54c25
Angie Liliana Cómbita Martínez <116600287+angieli13@users.noreply.github.com>
2023-02-02 12:06:27 -0500

    BORRE PRUEBA DE PROMESA

--- src/components/register.js
+++ src/components/register.js
@@ -48,20 +48,6 @@ export const Register = (onNavigate) => {
     }
     return true;
   }
-  const promise = new Promise((resolve, reject) => {
-    const number = Math.floor(Math.random() * 10);
-  
-    setTimeout(
-      () => number > 5
-        ? resolve(number)
-        : reject(new Error('Menor a 5')),
-      1000
-    );
-  });
-  
-  promise
-    .then(number => console.log(number))
-    .catch(error => console.error(error));
 
   buttonRegister.addEventListener('click', (e) => {
     e.preventDefault(); // No recargue la página
