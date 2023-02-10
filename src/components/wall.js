30734829b2fe240e9756aa8a85966f0328339841
angieli13 <angiequimica13@gmail.com>
2023-02-10 12:51:48 -0500

    quedamos en el diseño del muro y en el crear el nav barr static

--- src/components/wall.js
+++ src/components/wall.js
@@ -3,10 +3,21 @@ export const Wall = (onNavigate) => {
   main.setAttribute('class', 'contenedor');
 
   const muro = document.createElement('p');
-  muro.textContent = 'Este es el muro';
+  muro.textContent = 'Like Plants';
   muro.setAttribute('class', 'preguntaPassword');
+  
+  const div = document.createElement('div');
+  const buscador = document.createElement('input');
+  buscador.type = 'text';
+  buscador.setAttribute('class', 'lupa');
 
-  main.append(muro);
+  const nav = document.createElement('nav');
+
+  const ul = document.createElement('ul');
+
+
+  div.append(nav, ul, buscador);
+  main.append(muro, div);
 
   return main;
 };
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,40 +3,6 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from 'firebase/app';
-import
-
-{
-  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword,
-  signInWithPopup, GoogleAuthProvider,
-} from 'firebase/auth';
-// Enlazamos visual con firebase
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAXa8j4Ybc510kv68WeHddFaKFWOXkqhsM',
-  authDomain: 'likeplants-bc662.firebaseapp.com',
-  projectId: 'likeplants-bc662',
-  storageBucket: 'likeplants-bc662.appspot.com',
-  messagingSenderId: '1094663123741',
-  appId: '1:1094663123741:web:439a81d9b80773221527f4',
-  measurementId: 'G-LX9KPQYCP0',
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
-// Crear nueva cuenta pasando la dirección de correo electrónico y la contraseña del nuevo usuario
-const auth = getAuth(app); // constante para poder autenticar usuarios
-export const register = (email, password) => createUserWithEmailAndPassword(auth, email, password);
-export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
-
-// login con boton google
-const provider = new GoogleAuthProvider();
-export const loginWithGoogle = () => signInWithPopup(auth, provider);
-// aqui exportaras las funciones que necesites
-
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from 'firebase/app';
 import {
   getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword,
   signInWithPopup, GoogleAuthProvider,
