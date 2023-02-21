1c7e91b3a07649148dbae10da6b49df1964b34f3
angieli13 <angiequimica13@gmail.com>
2023-02-20 20:42:16 -0500

    rutas menu hamburguesa

---
+++ src/components/profile.js
@@ -0,0 +1,30 @@
+import { storage, saveFiles } from '../lib/firebase.js';
+
+export const Profile = (onNavigate) => {
+  const contenedor = document.createElement('div');
+  contenedor.setAttribute('class', 'contenedor');
+  contenedor.id = 'viewconteiner';
+
+  const fotoPerfil = document.createElement('img');
+
+  const inputPerfil = document.createElement('input');
+  // fotoPerfil.setAttribute('class', 'fotoPerfil');
+  inputPerfil.setAttribute('type', 'file');
+  inputPerfil.id = 'avatar';
+  inputPerfil.setAttribute('accept', 'image/png, image/jpeg');
+
+  inputPerfil.addEventListener('change', () => {
+    inputPerfil.files[0], inputPerfil.files[0];
+    alert('Guardado correctamente');
+    // const url = await readURL(inputPerfil.files[0]);
+    // fotoPerfil.src = url;
+    inputPerfil.classList.add('obj');
+    inputPerfil.file = inputPerfil.files[0];
+    console.log(inputPerfil.files);
+    saveFiles(inputPerfil.file, inputPerfil.file.name);
+  });
+
+  contenedor.append(fotoPerfil, inputPerfil);
+
+  return contenedor;
+};
